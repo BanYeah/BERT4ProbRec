64960f752d263172c39a8da042a17096d9e13293
BanYeah <04smailing@gmail.com>
2025-01-31 14:09:55 +0900

    HOTFIX

--- run_eval.sh
+++ run_eval.sh
@@ -1,4 +1,4 @@
-CKPT_DIR="./ckpt"
+CKPT="./ckpt"
 CKPT_DIR=$1
 
 dataset_name=$(echo "$CKPT_DIR" | sed 's/-mp.*//')
@@ -16,22 +16,25 @@ dupe_factor=$(echo "$CKPT_DIR" | sed 's/.*-df\([^-]*\)-mpps.*/\1/')
 signature="-mp${mask_prob}-sw${prop_sliding_window}-mlp${masked_lm_prob}-df${dupe_factor}-mpps${max_predictions_per_seq}-msl${max_seq_length}"
 
 
+# CUDA_VISIBLE_DEVICES=0 python -u run_eval.py \
+#     --test_input_file=./data/${dataset_name}${signature}.test.tfrecord \
+#     --vocab_filename=./data/${dataset_name}${signature}.vocab \
+#     --user_history_filename=./data/${dataset_name}${signature}.his \
+#     --checkpointDir=${CKPT}/${dataset_name} \
+#     --signature=${signature}-${dim} \
+#     --bert_config_file=./bert_train/bert_config_${dataset_name}_${dim}.json \
+#     --batch_size=${batch_size} \
+#     --max_seq_length=${max_seq_length} \
+#     --max_predictions_per_seq=${max_predictions_per_seq} \
+
 CUDA_VISIBLE_DEVICES=0 python -u run_eval.py \
-    --test_input_file=./data/${dataset_name}${signature}.test.tfrecord \
-    --vocab_filename=./data/${dataset_name}${signature}.vocab \
-    --user_history_filename=./data/${dataset_name}${signature}.his \
-    --checkpointDir=${CKPT_DIR}/${dataset_name} \
-    --signature=${signature}-${dim} \
-    --bert_config_file=./bert_train/bert_config_${dataset_name}_${dim}.json \
-    --batch_size=${batch_size} \
-    --max_seq_length=${max_seq_length} \
-    --max_predictions_per_seq=${max_predictions_per_seq} || python -u run_eval.py \
     --test_input_file=./data/learn-hist/${dataset_name}${signature}.test.tfrecord \
     --vocab_filename=./data/learn-hist/${dataset_name}${signature}.vocab \
     --user_history_filename=./data/learn-hist/${dataset_name}${signature}.his \
-    --checkpointDir=${CKPT_DIR}/${dataset_name} \
+    --checkpointDir=${CKPT}/${dataset_name} \
     --signature=${signature}-${dim} \
     --bert_config_file=./bert_train/bert_config_${dataset_name}_${dim}.json \
     --batch_size=${batch_size} \
     --max_seq_length=${max_seq_length} \
-    --max_predictions_per_seq=${max_predictions_per_seq}
+    --max_predictions_per_seq=${max_predictions_per_seq} \
+    
